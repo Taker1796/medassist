56047bcfc599100436ea22fb06337a3a60871269
user1 <taker1796@gmail.com>
2026-03-03 23:12:39 +0300

    адреса бэкенда

--- EnrichmentServiceUI/src/app/environments/environment.prod.ts
+++ EnrichmentServiceUI/src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const Environment = {
   production: true,
-  apiUrl: 'https://api.muk.i234.me:443',
+  apiUrl: 'https://enrich.muk.i234.me/',
   authUIUrlPath:'v1/auth-ui',
   promptTemplates:'v1/prompt-templates',
   enrichUrlPath:'v1/enrich',
--- EnrichmentServiceUI/src/app/environments/environment.ts
+++ EnrichmentServiceUI/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const Environment = {
   production: false,
-  apiUrl: 'https://nonusurpingly-frizzlier-rhiannon.ngrok-free.dev',
+  apiUrl: 'https://enrich.muk.i234.me',
   authUIUrlPath:'v1/auth-ui',
   promptTemplates:'v1/prompt-templates',
   enrichUrlPath:'v1/enrich',
