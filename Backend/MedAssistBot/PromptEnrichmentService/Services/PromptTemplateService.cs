26eb3b268771443dfd92523dad43ca61356100fa
user1 <taker1796@gmail.com>
2026-03-04 18:39:00 +0300

    правки обязательности пациента при обогащении

--- Backend/MedAssistBot/PromptEnrichmentService/Services/PromptTemplateService.cs
+++ Backend/MedAssistBot/PromptEnrichmentService/Services/PromptTemplateService.cs
@@ -22,7 +22,7 @@ public class PromptTemplateService
         _memoryCache = memoryCache;
     }
 
-    public async Task<EnrichedData> BuildEnrichedText(Guid patientId, string specialtyCode, Message[] messages, CancellationToken cancellationToken)
+    public async Task<EnrichedData> BuildEnrichedText(Guid? patientId, string specialtyCode, Message[] messages, CancellationToken cancellationToken)
     {
         var template = string.IsNullOrWhiteSpace(specialtyCode)
             ? await _promptTemplateRepository.GetDefaultAsync(cancellationToken)
@@ -33,13 +33,13 @@ public class PromptTemplateService
             throw new TemplateNotFoundException(specialtyCode);
         }
 
-        if (patientId == Guid.Empty)
+        if (!patientId.HasValue || patientId.Value == Guid.Empty)
         {
             return await BuildEnrichedMessages(template.Text, messages, cancellationToken: cancellationToken);
         }
         
 
-        var patientCard = await _patientCardRepository.GetByPatientIdAndSpecialtyAsync(patientId, specialtyCode, cancellationToken);
+        var patientCard = await _patientCardRepository.GetByPatientIdAndSpecialtyAsync(patientId.Value, specialtyCode, cancellationToken);
         if (patientCard == null)
         {
             return await BuildEnrichedMessages(template.Text, messages, cancellationToken: cancellationToken);
