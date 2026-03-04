a2cbc9f1b2aaee5f84a15e4fe30c617a08736816
user1 <taker1796@gmail.com>
2026-03-04 18:45:46 +0300

    правки обязательности пациента

--- Backend/MedAssistBot/PromptEnrichmentService/Controllers/Enrichment.cs
+++ Backend/MedAssistBot/PromptEnrichmentService/Controllers/Enrichment.cs
@@ -24,7 +24,13 @@ public class Enrichment : ControllerBase
     [ProducesResponseType(StatusCodes.Status401Unauthorized)] 
     public async Task<IActionResult> Post([FromBody] AddPromptRequest request, CancellationToken cancellationToken)
     {
-        var enrichedData = await _templateService.BuildEnrichedText(request.PatientId, request.DoctorSpecializationCode, request.Messages, cancellationToken);
+        Guid? patientId = null;
+        if (!string.IsNullOrWhiteSpace(request.PatientId) && Guid.TryParse(request.PatientId, out var parsedPatientId))
+        {
+            patientId = parsedPatientId;
+        }
+
+        var enrichedData = await _templateService.BuildEnrichedText(patientId, request.DoctorSpecializationCode, request.Messages, cancellationToken);
         var llmResponse = await _llmClient.SendAsync(enrichedData, cancellationToken);
         var enrichedText = FormatMessages(enrichedData.Messages);
 
