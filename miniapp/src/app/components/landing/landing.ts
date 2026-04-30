ea17303c695e3eb3ecd98e1710fa99c2504f8192
user1 <taker1796@gmail.com>
2026-04-30 23:38:02 +0300

    Refine marketing landing for desktop

--- miniapp/src/app/components/landing/landing.ts
+++ miniapp/src/app/components/landing/landing.ts
@@ -1,11 +1,7 @@
 import {Component} from '@angular/core';
-import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [
-    RouterLink
-  ],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
